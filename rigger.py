ec1dd84ce8b9ff403038edd81d965fe12e2c8df6
Rüçhan Adıgüzel <44341663+jceads@users.noreply.github.com>
2022-07-29 12:27:57 +0300

    hard coded variable names removed

--- rigger.py
+++ rigger.py
@@ -7,15 +7,16 @@ objects = bpy.data.objects
 data = bpy.data
 selector = bpy.ops.object
 ARMATURE_NAME = 'Armature.001'
-def init_objects():
+
+def init_objects(armature_name,coll_name):
     other_objects= []
-    
-    for item in data.collections['test-area'].all_objects:
+    #searching armature and other objects collection. Will be in script_ignore
+    for item in data.collections[coll_name].all_objects:
             other_objects.append(item)
     print('all objects initialized')
-    return bpy.data.objects[ARMATURE_NAME], other_objects
+    return bpy.data.objects[armature_name], other_objects
 
-def set_parent(parent,child):
+def set_parent():
     print('parenting to bone')
     selector.parent_set(type='ARMATURE_AUTO',keep_transform=True)
     
@@ -25,10 +26,10 @@ def select_arma(arma,others):
         item.select_set(True)
     
     arma.select_set(True)
-    set_parent(arma,others)
+    set_parent()
 
 
-def make_parent():
+def make_parent(ARMATURE_NAME):
     armature, other_objects = init_objects()
     select_arma(arma=armature,others=other_objects)
     
