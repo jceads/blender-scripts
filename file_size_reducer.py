cdb8d16f539fc5d63b7a736a10769ec1b2d68680
Rüçhan Adıgüzel <44341663+jceads@users.noreply.github.com>
2022-08-04 14:21:32 +0300

    description comments added

--- file_size_reducer.py
+++ file_size_reducer.py
@@ -3,8 +3,10 @@ import subprocess
 import os
 from PIL import Image
 
+#!SİSTEMDE FFMPEG YÜKLÜ OLMASI GEREKİYOR
+#!SCRİPT İLE OPTİMİZE EDİLECEK DOSYALARIN AYNI KLASÖRDE OLMASI GEREKİYOR
 
-
+#?Sistemde yüklü olan ffmpeg exe dosyasının yolu
 FFMPEG = "C:/ffmpeg/bin/ffmpeg"
 
 def calculateReso(image):
@@ -16,6 +18,7 @@ def calculateReso(image):
     
 
 def build_ffmpeg(fileName,resolution = 1024):
+    """Prepares ffmpeg command args"""
     resolution = calculateReso(fileName)
     scale = "scale="+str(resolution)+":"+str(resolution)
     outputFile = "_"+fileName
@@ -30,7 +33,6 @@ def build_ffmpeg(fileName,resolution = 1024):
     return commands_list
 
 def runFFmpeg(commands):
-
     if subprocess.run(commands).returncode == 0:
         print("files optimized succesfuly")
     else:
@@ -41,11 +43,12 @@ def optimezeFiles():
     images = search_for_images()
     for i in range(0,len(images)):
         runFFmpeg(build_ffmpeg(images[i]))
-        os.remove(images[i])
-        os.rename("_"+images[i],images[i])
+        os.remove(images[i])#orijinal yüksek boyutlu png leri siler
+        os.rename("_"+images[i],images[i])#optimize edilmiş pnglerin isminin başındaki "_"  işaretini kaldırır
 
 
 def search_for_images():
+    #!Script ile optimize edilecek png dosyalarının aynı konumda olması gerekir
     file_list =   []
     for file_name in os.listdir():
         if ".png" in file_name:
@@ -53,7 +56,6 @@ def search_for_images():
     return file_list
 
 def delete_all_images():
-    list_of_images = []
     for i in os.listdir():
         if ".png" in i:
             os.remove(i)
