9403385ae446fe3a6dac46696ed41b326b342473
Rüçhan Adıgüzel <44341663+jceads@users.noreply.github.com>
2022-08-04 14:24:22 +0300

    description comments added

--- glb_packer.py
+++ glb_packer.py
@@ -3,8 +3,9 @@ from pygltflib import GLTF2
 from pygltflib.utils import ImageFormat,glb2gltf,gltf2glb
 import os
 
-glb_file = None
+#!GLB DOSYASI İLE AYNI KLASÖRDE OLMASI GEREKİYOR
 
+glb_file = None
 for file in os.listdir():
     if ".glb" in file:
         glb_file = file
@@ -12,10 +13,8 @@ for file in os.listdir():
 
 gltf = GLTF2().load(glb_file)
 for image in range(0,len(gltf.images)):
-    gltf.export_image_to_file(image_index=image)
+    gltf.export_image_to_file(image_index=image)#GLB DOSYASINDAKİ PNG DOSYALARINI ÇIKARIR
 
-optimezeFiles()
+optimezeFiles()#ÇIKARILAN PNG LERİ OPTİMİZE EDER
 glb2gltf(glb_file)
-gltf2glb(source=glb_file,destination="optimized.glb")
-# gltf.images[0].uri = str(glb_file)[::-3]+"_image"  # will save the data uri to this file (regardless of data format)
-# gltf.convert_images(ImageFormat.FILE)
+gltf2glb(source=glb_file,destination="optimized.glb")#OPTİMİZE EDİLMİŞ HALDE TEKRAR GLB FORMATINA DÖNÜŞTÜRÜR !! TODO: OPTİMİZE EDİLMEMİŞ PNGLERİ KULLANMASINI DÜZELTME
